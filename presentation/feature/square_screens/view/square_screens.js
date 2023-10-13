6453d54c86f88c5f1f835d3718a1541e9762e006
Tung-SIH <tung-sih@MacBook-Pro.local>
2023-10-13 16:52:00 +0700

    ct

--- presentation/feature/square_screens/view/square_screens.js
+++ presentation/feature/square_screens/view/square_screens.js
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { View, Text, Button, FlatList } from "react-native";
 import ColorCounter from "../../components/colorcounter";
-const COLOR_INCREMENT = 15;
+
+
+
+// Các bước tạo useReducer
+// 1. Xác định initState ( dữ liệu mặc định)
+// 2. Action hành động
+// 3. Tạo Reducer
+// 4. Dispatch
+
 const SquareScreens = (props) => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
+  // const [red, setRed] = useState(0);
+  // const [green, setGreen] = useState(0);
+  // const [blue, setBlue] = useState(0);
   console.log(red);
+  const inistate = {red: 0, blue: 0 ,green: 0};
+
+  const reducer = (color, action) = {
+    
+  }
   const setColor = (color, change) => {
-    if (color === "red") {
-      if (red + change > 255 || red + change < 0) {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
         return;
-      } else {
-        setRed(red + change);
-      }
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
     }
   };
   return (
@@ -33,14 +54,14 @@ const SquareScreens = (props) => {
         color="Red"
       />
       <ColorCounter
-        onPress={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
-        color="Blue"
+        onPress={() => setColor("blue", COLOR_INCREMENT)}
+        onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
+        color="blue"
       />
       <ColorCounter
-        onPress={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
-        color="Green"
+        onPress={() => setColor("green", COLOR_INCREMENT)}
+        onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
+        color="green"
       />
     </View>
   );
